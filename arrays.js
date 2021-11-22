6eac8eac1ffb17139f8a26a73a0147ec0731faa4
Matt Zimmer <zimmermja@gmail.com>
2021-11-22 21:29:55 +0000

    added length-checker to arrays.js

--- arrays.js
+++ arrays.js
@@ -5,6 +5,14 @@ const addToBatch = (array, number) => {
   return array;
 };
 
-console.log(addToBatch([1,2,3,4], 5))
-console.log(addToBatch([], 8))
-console.log(addToBatch([1, 2, 3, 4, 5, 6], 7))
+// console.log(addToBatch([1,2,3,4], 5))
+// console.log(addToBatch([], 8))
+// console.log(addToBatch([1, 2, 3, 4, 5, 6], 7))
+
+const checkLength = (number) => {
+  return number.length <= 10;
+};
+
+const filterLongNumbers = (phone_numbers) => {
+  return phone_numbers.filter(checkLength);
+};
