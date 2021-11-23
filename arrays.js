08520826cad585a5e7ceec21294153621511639b
Matt Zimmer <zimmermja@gmail.com>
2021-11-23 10:54:40 +0000

    implemented namesAndDiscounts

--- arrays.js
+++ arrays.js
@@ -19,10 +19,18 @@ const filterLongNumbers = (phone_numbers) => {
 
 const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
 
-const generateMessages = (names) => {
-  return names.map(name => {
-    return `Hi ${name}! 50% off our best candies for you today!`
+const namesAndDiscounts = [
+  ['Anna', 50],
+  ['Laura', 40],
+  ['Josh', 30],
+  ['Min', 50],
+  ['Karla', 60]
+];
+
+const generateMessages = (namesAndDiscounts) => {
+  return namesAndDiscounts.map(nad => {
+    return `Hi ${nad[0]}! ${nad[1]}% off our best candies for you today!`
   });
 };
 
-console.log(generateMessages(names));
+console.log(generateMessages(namesAndDiscounts));
